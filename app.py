de4d9c16cb7ffdc54305ebd53cb8846133ff3032
Denis Smirnov <upccore@list.ru>
2026-06-13 11:35:14 +0300

    black and isort

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import grpc
 from concurrent import futures
+
+import grpc
 
 import kvstore_pb2_grpc
 from servicer import KeyValueStoreServicer
@@ -7,7 +8,9 @@ from servicer import KeyValueStoreServicer
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    kvstore_pb2_grpc.add_KeyValueStoreServicer_to_server(KeyValueStoreServicer(), server)
+    kvstore_pb2_grpc.add_KeyValueStoreServicer_to_server(
+        KeyValueStoreServicer(), server
+    )
     server.add_insecure_port("[::]:8000")
     server.start()
     server.wait_for_termination()
